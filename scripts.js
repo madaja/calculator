3e1d54df465089e44b048b2ccd263df103737b6a
Matt Jasinski <madaja1714@gmail.com>
2020-02-14 16:59:58 -0500

    Added multiplication and division.

--- scripts.js
+++ scripts.js
@@ -15,23 +15,24 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-    if (operator === '+') {
+    if (operator === 'add') {
         return add(a, b);
     }
-    if (operator === '-') {
+    if (operator === 'subtract') {
         return subtract(a, b);
     }
-    if (operator === '*') {
+    if (operator === 'multiply') {
         return multiply(a, b);
     }
-    if (operator === '/') {
+    if (operator === 'divide') {
         return divide(a, b);
     }
 }
 let numStr = '';
-let num;
+let num, runningTotal;
 let numArray = [];
 let opArray = [];
+let opIndex;
 let screen = document.getElementById('calc-screen');
 
 let digits = document.querySelectorAll('.digit');
@@ -62,6 +63,41 @@ function operatorClick() {
 
 function runEquals() {
     storeNum();
+    let i = 0;
+    opArray.forEach(function(element){
+        
+        opIndex = opArray.indexOf(element);
+        console.log(opIndex);
+        if (element === 'multiply'|| element == 'divide') {
+            if (opIndex === 0) {
+               runningTotal = operate(element, numArray[opIndex], numArray[opIndex + 1]);
+            } 
+            else {
+            runningTotal = operate(element, runningTotal, numArray[opIndex + 1]);
+            console.log(runningTotal);
+            }
+            opArray[opIndex] = 'cleared' + i;
+            i++
+        
+        }
+    });
+    /* opArray.forEach(function(element){
+        opIndex = opArray.indexOf(element);
+        console.log(opIndex);
+        if (element === 'add'|| element == 'subtract') {
+            if (opIndex === 0) {
+               runningTotal = operate(element, numArray[opIndex], numArray[opIndex + 1]);
+            } 
+            else {
+            runningTotal = operate(element, runningTotal, numArray[opIndex + 1]);
+            console.log(runningTotal);
+            }
+            opArray[opIndex] = 'cleared' + i;
+            i++
+        
+        }
+    }); */
+    console.log(runningTotal);
 }
 
 function storeNum() {
